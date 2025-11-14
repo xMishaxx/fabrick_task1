838fdfe496193c504c2ac9b157ae258b2a39c1b5
Mihail-Fricatel <mihail.fricatel@biters.it>
2025-11-14 10:02:05 +0100

    spring boot starter project

---
+++ src/test/java/com/example/fabrick_task1/FabrickTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fabrick_task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
